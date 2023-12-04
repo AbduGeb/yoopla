201412a28a0bb50c4a67d49f7ba969288e31f181
AbduGeb <abdu.gebril@gmail.com>
2023-12-04 22:37:09 +0000

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h2>Yoopla</h2>
+    </div>
+  );
+};
+
+export default App;
